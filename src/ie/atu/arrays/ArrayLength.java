d4fa2eaccf92751fa0786d0399aaa36f4446868d
Min Kyaw <mintheinkyawprd@gmail.com>
2025-10-23 15:35:20 +0000

    Add ArrayLength.java and RainbowLength.java to demonstrate array length retrieval

---
+++ src/ie/atu/arrays/ArrayLength.java
@@ -0,0 +1,8 @@
+package ie.atu.arrays;
+
+public class ArrayLength {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println("The length of the array is: " + numbers.length);
+    }
+}
---
+++ src/ie/atu/arrays/RainbowLength.java
@@ -0,0 +1,8 @@
+package ie.atu.arrays;
+
+public class RainbowLength {
+    public static void main(String[] args) {
+        String[] colors = {"Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"};
+        System.out.println("Number of colors: " + colors.length);
+    }
+}
